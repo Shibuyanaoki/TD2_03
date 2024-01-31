edde27c15abff70d741cf06e79acffece045e249
Oguro Haruka <Oguro Haruka@MSI>
2024-01-31 14:05:25 +0900

    q

---
+++ DirectXGame/Spark.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+class Spark {
+public:
+	void Initialize(Model* model);
+	void Update();
+	void Draw(const ViewProjection& viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+};
