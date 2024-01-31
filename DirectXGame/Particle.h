a56732fa14e827984d14db4f1450f2306650bb77
Oguro Haruka <Oguro Haruka@MSI>
2024-01-31 14:02:58 +0900

    q

--- DirectXGame/Particle.h
+++ DirectXGame/Particle.h
@@ -1,3 +1,25 @@
 #pragma once
+#include "MT.h"
+#include "Model.h"
+#include "WorldTransform.h"
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
 class Particle {
-void};
+public:
+	void Initialize(Model* model, const Vector3& position);
+	void Update();
+	void Draw(const ViewProjection& viewProjection);
+	bool IsDelete() const { return isDelete_; }
+
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	static const int32_t kLifeTime = 30;
+	int32_t deathTimer_ = kLifeTime;
+	bool isDelete_ = false;
+	Vector3 velocity_;
+	float speed;
+	float random;
+};
